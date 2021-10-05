a05bde232e4ca10dd6fdb43493aa86d50fd7c003
Claudio Gomes <claudio.gomes@eng.au.dk>
2021-10-05 08:22:30 +0200

    Add all non live tests.

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     parser.add_argument(
         "--run-tests",
         dest="run_tests",
-        choices=["api","cli","slow_tests","live"],
+        choices=["api","cli","slow_tests","live", "all-non-live"],
         help="when set, the dynamic calibration tests will be run, depending on the choice: 'api','cli','slow_tests','live'. Live tests can be performed on a UR robot."
     )
     args = parser.parse_args()
@@ -30,6 +30,12 @@ if __name__ == '__main__':
     elif args.run_tests ==  "cli":
         suite = unittest.TestSuite()
         suite.addTest(unittest.TestLoader().discover("aurt/tests", pattern="cli_tests.py"))
+        runner = unittest.TextTestRunner()
+        res = runner.run(suite)
+    elif args.run_tests == "all-non-live":
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.TestLoader().discover("aurt/tests", pattern="*test*.py"))
+        suite.addTest(unittest.TestLoader().discover("tests/slow_tests", pattern="*test*.py"))
         runner = unittest.TextTestRunner()
         res = runner.run(suite)
     elif args.run_tests == "live":
